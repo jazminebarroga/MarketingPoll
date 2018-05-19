57886a48247fe036345616175b737bad9aacd9a4
Jazmine Paola Barroga <jpbbarroga@gmail.com>
2018-05-19 14:13:50 +0800

    Add custom thumb for seekbar

--- app/src/main/java/com/jpbbarroga/marketingpoll/SmsParser.kt
+++ app/src/main/java/com/jpbbarroga/marketingpoll/SmsParser.kt
@@ -6,17 +6,17 @@ package com.jpbbarroga.marketingpoll
 
 class SmsParser {
 
-    fun parseSms(sms: String): Pair<Person, String> {
-        val strings = sms.split(' ', true, 3)
-        var person: Person?
-        if (strings[0].equals("BB", true)) {
-            if (strings[1].equals(Person.HANS.firstName, true)) {
-                person = Person.HANS
-            } else if (strings[1].equals(Person.LEAN.firstName, true)) {
-                person = Person.LEAN
-            } else if (strings[1].equals(Person.MIKKO.firstName, true)) {
-                person = Person.MIKKO
-            }
-        }
-    }
+//    fun parseSms(sms: String): Pair<Person, String> {
+//        val strings = sms.split(" ", true, 3)
+//        var person: Person?
+//        if (strings[0].equals("BB", true)) {
+//            if (strings[1].equals(Person.HANS.firstName, true)) {
+//                person = Person.HANS
+//            } else if (strings[1].equals(Person.LEAN.firstName, true)) {
+//                person = Person.LEAN
+//            } else if (strings[1].equals(Person.MIKKO.firstName, true)) {
+//                person = Person.MIKKO
+//            }
+//        }
+//    }
 }
