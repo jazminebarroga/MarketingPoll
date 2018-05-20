42e33b64fca718c0575822ed5759e86f574fa36b
Jazmine Paola Barroga <jpbbarroga@gmail.com>
2018-05-20 21:08:52 +0800

    Added flashbar

--- app/src/main/java/com/jpbbarroga/marketingpoll/MainActivity.kt
+++ app/src/main/java/com/jpbbarroga/marketingpoll/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.jpbbarroga.marketingpoll
 
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -8,8 +12,12 @@ import android.os.Bundle
 import android.provider.Telephony
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.telephony.SmsMessage
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
+import com.andrognito.flashbar.Flashbar
+import com.andrognito.flashbar.anim.FlashAnim
 import java.util.jar.Manifest
 
 //data class Person(val name: String, val color: String, var numVotes: Int)
@@ -22,7 +30,7 @@ enum class Person(val firstName: String, val color: String, var numVotes: Int) {
 }
 class MainActivity : AppCompatActivity() {
 
-    private var smsReceiver: SmsReceiver = SmsReceiver()
+    private lateinit var smsReceiver: BroadcastReceiver
 
     private lateinit var listView: ListView
 
@@ -95,7 +103,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configureReceiver() {
+
+        smsReceiver = object: BroadcastReceiver() {
+
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val cont = context?.let { it } ?: return
+                val i = intent?.let { it } ?: return
+
+                val smsBundle = i.extras
+                if (smsBundle != null) {
+                    val pdus = smsBundle.get("pdus") as Array<Any>
+                    for (onePdus : Any in pdus) {
+//                        val oneSMS = SmsMessage.createFromPdu(onePdus as ByteArray)
+//                        str += "SMS from " +oneSMS.originatingAddress
+//                        str += " :"
+//                        str += oneSMS.messageBody.toString()
+//                        str += "\n"
+                        Flashbar.Builder(this@MainActivity)
+                                .gravity(Flashbar.Gravity.BOTTOM)
+                                .title("HELLO WORLD")
+                                .message("YOU RECEIVED A NEW TEXT MESSAGE")
+                                .backgroundColorRes(R.color.colorAccent)
+                                .showOverlay().enterAnimation(FlashAnim.with(cont)
+                                .animateBar()
+                                .duration(750)
+                                .alpha()
+                                .overshoot())
+                                .exitAnimation(FlashAnim.with(cont)
+                                        .animateBar()
+                                        .duration(400)
+                                        .accelerateDecelerate())
+                                .build()
+                    }
+               }
+            }
+        }
+
         registerReceiver(smsReceiver,  IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION))
+
 
     }
 
